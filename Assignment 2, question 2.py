78b601d49858a6a1e07fb3638edd6e1b2c0d6d00
AnushkaGupta-21107103 <103170202+AnushkaGupta-21107103@users.noreply.github.com>
2022-05-06 23:01:42 +0530

    Delete Assignment 2, question 2.py

--- Assignment 2, question 2.py
+++
@@ -1,6 +0,0 @@
-Name='Anushka Gupta'
-SID=21107103
-Department='Mechanical'
-CGPA=9.9
-
-print('Hey, '+Name+' Here!\nMy SID is ' + str(SID)+'\nI am from '+Department+' Department and my CGPA is '+str(CGPA))
