f990789029e03634fc252d0bb12fa94e83cf2523
AnushkaGupta-21107103 <103170202+AnushkaGupta-21107103@users.noreply.github.com>
2022-05-06 21:43:00 +0530

    Add files via upload

---
+++ Assignment 2, question 2.py
@@ -0,0 +1,6 @@
+Name='Anushka Gupta'
+SID=21107103
+Department='Mechanical'
+CGPA=9.9
+
+print('Hey, '+Name+' Here!\nMy SID is ' + str(SID)+'\nI am from '+Department+' Department and my CGPA is '+str(CGPA))
